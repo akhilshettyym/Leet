9cb26e4c38932ded9673bc771bd55a896e4744e1
akhilshettyym <akhilshettym2003@gmail.com>
2025-08-22 21:08:29 +0530

    Added extra input to check for diff cases

--- Easy/A_TwoSum.java
+++ Easy/A_TwoSum.java
@@ -7,6 +7,12 @@ public class A_TwoSum {
         int[] nums = { 2, 7, 11, 15};
         int target = 9;
 
+        // int[] nums = { 3, 3 };
+        // int target = 6;
+
+        // int[] nums = { 3, 3};
+        // int target = 6;
+
         int[] test = twoSum(nums, target);
         System.out.println(Arrays.toString(test));
     }
