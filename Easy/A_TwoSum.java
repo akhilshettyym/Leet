0d66c1614e3b1e1e26c399af2015fc23afc347cf
akhilshettyym <akhilshettym2003@gmail.com>
2025-08-22 21:07:47 +0530

    working code

--- Easy/A_TwoSum.java
+++ Easy/A_TwoSum.java
@@ -1,7 +1,24 @@
 package akhilshettyym.Leet.Easy;
 
+import java.util.Arrays;
+
 public class A_TwoSum {
     public static void main(String[] args) {
-        
+        int[] nums = { 2, 7, 11, 15};
+        int target = 9;
+
+        int[] test = twoSum(nums, target);
+        System.out.println(Arrays.toString(test));
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; i < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[] { i, j };
+                }
+            }
+        }
+        return new int[] { -1, -1 };
     }
 }
