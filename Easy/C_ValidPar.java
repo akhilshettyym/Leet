d4ab2429c343aa3b1f95d42b1f24a9003d8d663b
akhilshettyym <akhilshettym2003@gmail.com>
2025-08-25 14:59:11 +0530

    Two Sum indices

--- Easy/C_ValidPar.java
+++ Easy/C_ValidPar.java
@@ -1,30 +1,44 @@
 package akhilshettyym.Leet.Easy;
 
 import java.util.Stack;
-// Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
 
-// An input string is valid if:
-
-// Open brackets must be closed by the same type of brackets.
-// Open brackets must be closed in the correct order.
-// Every close bracket has a corresponding open bracket of the same type.
 class Main {
-    
-    public static void main (String[] args){
-        String str = "([])";
-        Stack<Character> charStack = new Stack<>();
-        for(char c : str.toCharArray()) {
-            if(c == '(' || c == '{' || c == '['){
-            charStack.push(c);
-            System.out.println(c);
-            }
-            if(c == ')' || c == '}' || c == ']'){
-                char topEle = Stack.peek();
-                if(topEle == '(' || c == ')'){
-                    System.out.println(c);
-                }
-            }
-        }
-        // System.out.print("Stack :"+ charStack);
+   public static boolean main(String[] args) {
+       String str = "([])";
+       Stack<Character> charStack = new Stack<>();
+
+       for (char c : str.toCharArray()) {
+           if (c == '(' || c == '{' || c == '[') {
+               charStack.push(c);
+           } else if (c == ')' || c == '}' || c == ']') {
+               if (charStack.isEmpty()) {
+                   return false;
+               }
+               char top = charStack.pop();
+               if (!isMatchingPair(top, c)) {
+                   return false;
+               }
+           }
+       }
+
+       if (charStack.isEmpty()) {
+           return true;
+       } else {
+           return false;
+       }
+   }
+
+   private static boolean isMatchingPair(char open, char close) {
+       return (open == '(' && close == ')') ||
+              (open == '{' && close == '}') ||
+              (open == '[' && close == ']');
+   }
+}
+
+
+// I want the above in below format
+class Solution {
+    public boolean isValid(String s) {
+        
     }
 }
