ffc8fafca435e5c794f791c01917a0cff47c105c
akhilshettyym <akhilshettym2003@gmail.com>
2025-08-26 18:26:32 +0530

    Palindrome linked list

--- Easy/PalLinkedL.java
+++ Easy/PalLinkedL.java
@@ -1,56 +1,78 @@
 package akhilshettyym.Leet.Easy;
 
+// Given the head of a singly linked list, return true if it is a palindrome or false otherwise.
+// Runtime 6ms
 public class PalLinkedL {
-    /**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-class Solution {
-    public ListNode findMiddle(ListNode head) {
-        ListNode hare = head;
-        ListNode turtle = head;
+    public static class ListNode {
+        int val;
+        ListNode next;
 
-        while(hare.next != null && hare.next.next != null){
-            hare = hare.next.next;
-            turtle = turtle.next;
+        ListNode() {
         }
-        return turtle;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
     }
 
-    public ListNode reverse(ListNode head) {
-        ListNode prev = null;
-        ListNode curr = head;
-        
-        while(curr != null) {
-            ListNode next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
+    static class Solution {
+        public ListNode findMiddle(ListNode head) {
+            ListNode hare = head;
+            ListNode turtle = head;
+
+            while (hare.next != null && hare.next.next != null) {
+                hare = hare.next.next;
+                turtle = turtle.next;
+            }
+            return turtle;
         }
-        return prev;
-    }
-    public boolean isPalindrome(ListNode head) {
-        if(head == null || head.next == null) {
+
+        public ListNode reverse(ListNode head) {
+            ListNode prev = null;
+            ListNode curr = head;
+
+            while (curr != null) {
+                ListNode next = curr.next;
+                curr.next = prev;
+                prev = curr;
+                curr = next;
+            }
+            return prev;
+        }
+
+        public boolean isPalindrome(ListNode head) {
+            if (head == null || head.next == null) {
+                return true;
+            }
+            ListNode middle = findMiddle(head);
+            ListNode secondHalfStart = reverse(middle.next);
+
+            ListNode firstHalfStart = head;
+            while (secondHalfStart != null) {
+                if (firstHalfStart.val != secondHalfStart.val) {
+                    return false;
+                }
+                firstHalfStart = firstHalfStart.next;
+                secondHalfStart = secondHalfStart.next;
+            }
             return true;
         }
-        ListNode middle = findMiddle(head);
-        ListNode secondHalfStart = reverse(middle.next);
-
-        ListNode firstHalfStart = head;
-        while(secondHalfStart != null) {
-            if(firstHalfStart.val != secondHalfStart.val) {
-                return false;
-            }
-            firstHalfStart = firstHalfStart.next;
-            secondHalfStart = secondHalfStart.next;
-        }
-        return true;
     }
-}
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(2);
+        head.next.next.next = new ListNode(1);
+
+        Solution sol = new Solution();
+        boolean result = sol.isPalindrome(head);
+
+        System.out.println("Is Palindrome? " + result);
+    }
 }
