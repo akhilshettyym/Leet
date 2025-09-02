d72bd5f222f0ce99d6e572fe600e39d8b434f28a
akhilshettyym <akhilshettym2003@gmail.com>
2025-09-02 17:10:41 +0530

    Valid palindrome strings

--- Easy/ValidPalindrome_125.java
+++ Easy/ValidPalindrome_125.java
@@ -1,16 +1,26 @@
 package akhilshettyym.Leet.Easy;
 
-/*A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+/*A phrase is a palindrome if, after converting all uppercase letters into lowercase letters 
+and removing all non-alphanumeric characters, it reads the same forward and backward.
+Runtime 3ms */
 
-Given a string s, return true if it is a palindrome, or false otherwise.*/
 public class ValidPalindrome_125 {
     public boolean isPalindrome(String s) {
         int left = 0;
         int right = s.length() - 1;
+
         while (left < right) {
-            if (s.charAt(left) != s.charAt(right)) {
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+
+            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
                 return false;
             }
+
             left++;
             right--;
         }
@@ -21,8 +31,8 @@ public class ValidPalindrome_125 {
         ValidPalindrome_125 sol = new ValidPalindrome_125();
 
         System.out.println("madam → " + sol.isPalindrome("madam"));
-        System.out.println("racecar → " + sol.isPalindrome("race a car"));
-        System.out.println("heheh → " + sol.isPalindrome("a man a plan a canal panama"));
-
+        System.out.println("race a car → " + sol.isPalindrome("race a car"));
+        System.out.println("A man, a plan, a canal: Panama → " + sol.isPalindrome("A man, a plan, a canal: Panama"));
+        System.out.println("No 'x' in Nixon → " + sol.isPalindrome("No 'x' in Nixon"));
     }
 }
