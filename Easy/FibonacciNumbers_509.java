0353399182fcc2b2e22dabbedc7346bf0c07fe8b
akhilshettyym <akhilshettym2003@gmail.com>
2025-09-03 22:02:43 +0530

    Fibonacci number

---
+++ Easy/FibonacciNumbers_509.java
@@ -0,0 +1,31 @@
+package akhilshettyym.Leet.Easy;
+/*The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the 
+sum of the two preceding ones, starting from 0 and 1. That is,
+
+F(0) = 0, F(1) = 1
+F(n) = F(n - 1) + F(n - 2), for n > 1.
+Given n, calculate F(n). 
+Runtime 10ms */
+
+import java.util.Scanner;
+
+public class FibonacciNumbers_509 {
+    public static int fib(int n) {
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else {
+            return fib(n - 1) + fib(n - 2);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int n = sc.nextInt();
+        int result = fib(n);
+        System.out.println("Fibonacci number : " + result);
+        sc.close();
+    }
+}
