aaa287036f0b3ddfa5a8783a61559ddac60a38a9
akhilshettyym <akhilshettym2003@gmail.com>
2025-09-08 19:14:09 +0530

    Merging two linked lists

--- Easy/MergeTwoLists_21.java
+++ Easy/MergeTwoLists_21.java
@@ -1,9 +1,19 @@
 package akhilshettyym.Leet.Easy;
+
 /* You are given the heads of two sorted linked lists list1 and list2.
+  Merge the two lists into one sorted list. 
+  The list should be made by splicing together the nodes of the first two lists.
+  Return the head of the merged linked list. 
+*/
 
-Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+class ListNode {
+    int val;
+    ListNode next;
 
-Return the head of the merged linked list. */
+    ListNode(int val) {
+        this.val = val;
+    }
+}
 
 public class MergeTwoLists_21 {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
@@ -21,7 +31,27 @@ public class MergeTwoLists_21 {
         }
     }
 
-    public static void main(String[] args) {
+    public static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + (head.next != null ? " -> " : ""));
+            head = head.next;
+        }
+        System.out.println();
+    }
 
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(2);
+        l1.next.next = new ListNode(4);
+
+        ListNode l2 = new ListNode(1);
+        l2.next = new ListNode(3);
+        l2.next.next = new ListNode(4);
+
+        MergeTwoLists_21 sol = new MergeTwoLists_21();
+        ListNode merged = sol.mergeTwoLists(l1, l2);
+
+        System.out.print("Merged List: ");
+        printList(merged);
     }
 }
