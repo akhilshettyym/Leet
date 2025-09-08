a37c1027456908cbd9a5c67f170262653e521f2c
akhilshettyym <akhilshettym2003@gmail.com>
2025-09-08 18:26:52 +0530

    Remove Duplicates from Sorted array

---
+++ Easy/RemoveDuplicates_26.java
@@ -0,0 +1,30 @@
+package akhilshettyym.Leet.Easy;
+
+/* Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+
+Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
+
+Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially. The remaining elements of nums are not important as well as the size of nums.
+Return k.
+Runtime 1ms */
+public class RemoveDuplicates_26 {
+    public static int removeDuplicates(int[] nums) {
+        if (nums.length == 0)
+            return 0;
+        int k = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+        int result = removeDuplicates(nums);
+
+        System.out.println("Result : " + result);
+    }
+}
