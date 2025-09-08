90fa9f0cbfee214e84d60193190f72953e96b8e2
akhilshettyym <akhilshettym2003@gmail.com>
2025-09-08 20:01:42 +0530

    Find the middle of the linked list

---
+++ Easy/MiddleLinkedList_876.java
@@ -0,0 +1,42 @@
+package akhilshettyym.Leet.Easy;
+
+public class MiddleLinkedList_876 {
+    class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        if(fast.next != null && slow.next.next != null) {
+            fast = fast.next;
+            slow = slow.next.next;
+        }
+        return slow;
+    }
+}
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        System.out.print("Original List: ");
+        printList(head);
+
+        ReverseLL_206 outer = new ReverseLL_206();
+        Object sol = outer.new Solution();
+        ListNode middleNode = ((Solution) sol).middleNode(head);
+
+        System.out.print("Middle of the List: ");
+        printList(middleNode);
+    }
+
+    public static void printList(ListNode head) {
+        ListNode curr = head;
+        while (curr != null) {
+            System.out.print(curr.val + " ");
+            curr = curr.next;
+        }
+        System.out.println();
+    }
+}
