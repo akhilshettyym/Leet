60e433f1f75b7faa93f0aa3abedd7504ac68b67d
akhilshettyym <akhilshettym2003@gmail.com>
2025-09-11 20:35:09 +0530

    Implementation of stack using 2 queues

---
+++ Easy/StackUsingQueue_225.java
@@ -0,0 +1,112 @@
+package akhilshettyym.Leet.Easy;
+
+/* Implement a last-in-first-out (LIFO) stack using only two queues. The implemented stack should support all the functions of a normal stack (push, top, pop, and empty).
+
+Implement the MyStack class:
+
+void push(int x) Pushes element x to the top of the stack.
+int pop() Removes the element on the top of the stack and returns it.
+int top() Returns the element on the top of the stack.
+boolean empty() Returns true if the stack is empty, false otherwise.
+Notes:
+
+You must use only standard operations of a queue, which means that only push to back, peek/pop from front, size and is empty operations are valid.
+Depending on your language, the queue may not be supported natively. You may simulate a queue using a list or deque (double-ended queue) as long as you use only a queue's standard operations.
+Runtime 0ms
+
+LeetCode -
+class MyStack {
+    Queue<Integer> q1;
+    Queue<Integer> q2;
+
+    public MyStack() {
+        q1 = new LinkedList<>();
+        q2 = new LinkedList<>();
+    }
+    
+    public void push(int x) {
+        while(!q1.isEmpty()) {
+            q2.add(q1.remove());
+        }
+        q1.add(x);
+        while(!q2.isEmpty()) {
+            q1.add(q2.remove());
+        }
+    }
+    
+    public int pop() {
+        if(q1.isEmpty()){
+            return -1;
+        }
+        return q1.remove();
+    }
+    
+    public int top() {
+        if(q1.isEmpty()) {
+            return -1;
+        }
+        return q1.peek();
+    }
+    
+    public boolean empty() {
+        return q1.isEmpty();
+    }
+}   */
+import java.util.*;
+
+public class StackUsingQueue_225 {
+
+    Queue<Integer> q1;
+    Queue<Integer> q2;
+
+    public StackUsingQueue_225() {
+        q1 = new LinkedList<>();
+        q2 = new LinkedList<>();
+    }
+
+    public void push(int x) {
+        while (!q1.isEmpty()) {
+            q2.add(q1.remove());
+        }
+        q1.add(x);
+        while (!q2.isEmpty()) {
+            q1.add(q2.remove());
+        }
+    }
+
+    public int pop() {
+        if (q1.isEmpty()) {
+            return -1;
+        }
+        return q1.remove();
+    }
+
+    public int top() {
+        if (q1.isEmpty()) {
+            return -1;
+        }
+        return q1.remove();
+    }
+
+    public boolean empty() {
+        return q1.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        StackUsingQueue_225 q = new StackUsingQueue_225();
+
+        q.push(10);
+        q.push(20);
+        q.push(30);
+        q.push(40);
+
+        System.out.println("Top: " + q.top());
+        System.out.println("remove: " + q.pop());
+        System.out.println("Peek after remove: " + q.top());
+        System.out.println("Empty? " + q.empty());
+
+        q.pop();
+        q.pop();
+        System.out.println("Empty after removing all: " + q.empty());
+    }
+}
