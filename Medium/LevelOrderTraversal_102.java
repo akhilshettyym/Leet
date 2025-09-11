7ee17e35c86b3a7c21dbb494cd8a61ada294e982
akhilshettyym <akhilshettym2003@gmail.com>
2025-09-11 15:57:15 +0530

    Level order traversal

---
+++ Medium/LevelOrderTraversal_102.java
@@ -0,0 +1,104 @@
+package akhilshettyym.Leet.Medium;
+
+/* Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).
+Runtime 1ms
+
+LeetCode - 
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+
+        while (!q.isEmpty()) {
+            int size = q.size();
+            List<Integer> level = new ArrayList<>();
+
+            for (int i = 0; i < size; i++) {
+                TreeNode currNode = q.remove();
+                level.add(currNode.val);
+
+                if (currNode.left != null) {
+                    q.add(currNode.left);
+                }
+                if (currNode.right != null) {
+                    q.add(currNode.right);
+                }
+            }
+            result.add(level);
+        }
+        return result;
+    }
+}
+*/
+
+import java.util.*;
+
+public class LevelOrderTraversal_102 {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static class BinaryTree {
+        static int idx = -1;
+
+        public static Node buildNode(int nodes[]) {
+            idx++;
+            if (nodes[idx] == -1) {
+                return null;
+            }
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildNode(nodes);
+            newNode.right = buildNode(nodes);
+
+            return newNode;
+        }
+    }
+
+    public static void levelOrder(Node root) {
+        if (root == null) {
+            return;
+        }
+
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        q.add(null);
+        while (!q.isEmpty()) {
+            Node currNode = q.remove();
+            if (currNode == null) {
+                System.out.println();
+                if (q.isEmpty()) {
+                    break;
+                } else {
+                    q.add(null);
+                }
+            } else {
+                System.out.print(currNode.data + " ");
+                if (currNode.left != null) {
+                    q.add(currNode.left);
+                }
+                if (currNode.right != null) {
+                    q.add(currNode.right);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+        Node root = BinaryTree.buildNode(nodes);
+        levelOrder(root);
+    }
+}
